4ecbb8700e4bf0294d7e12e9db5a82cf2ec7710e
Joshua Martin <martin.joshua2410@gmail.com>
2024-10-04 14:18:53 +0100

    added dotenv

--- functions/connection.js
+++ functions/connection.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./permissions.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 exports.db = admin.firestore();
