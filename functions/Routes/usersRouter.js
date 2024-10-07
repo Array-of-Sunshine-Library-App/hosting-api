f2adbf5d518865d6ad593f10160341340f92f8f3
J-greaves <jacksongreaves@googlemail.com>
2024-10-07 09:34:38 +0100

    added get all users endpoint

--- functions/Routes/usersRouter.js
+++ functions/Routes/usersRouter.js
@@ -1,49 +1,75 @@
 "use strict";
 const express = require("express");
-const { postUser, getUserById, postBookLibrary, getAllBooksByUsername, postBookWishList, getAllWishListByUsername, getBookById, getWishlistBookById, deleteBookById, deleteWishlistBookById, postFriendRequest, postAceptFriendRequest, getFriendsList, getFriendRequestsList, requestBookToBorrow, getRequestsByBook, postAcceptedRequest, getLending, deleteFriendRequestById, deleteBorrowRequest, returnBookById, getBorrowing, getEndpoints } = require("../Controllers/usersControllers");
+const {
+  postUser,
+  getUserById,
+  postBookLibrary,
+  getAllBooksByUsername,
+  postBookWishList,
+  getAllWishListByUsername,
+  getBookById,
+  getWishlistBookById,
+  deleteBookById,
+  deleteWishlistBookById,
+  postFriendRequest,
+  postAceptFriendRequest,
+  getFriendsList,
+  getFriendRequestsList,
+  requestBookToBorrow,
+  getRequestsByBook,
+  postAcceptedRequest,
+  getLending,
+  deleteFriendRequestById,
+  deleteBorrowRequest,
+  returnBookById,
+  getBorrowing,
+  getEndpoints,
+  getAllUsers,
+} = require("../Controllers/usersControllers");
 const usersRouter = express.Router();
+usersRouter.route("/allusers").get(getAllUsers);
 usersRouter.route("/newuser").post(postUser);
 usersRouter.route("/:username").get(getUserById);
 usersRouter
-    .route("/:username/books")
-    .post(postBookLibrary)
-    .get(getAllBooksByUsername);
+  .route("/:username/books")
+  .post(postBookLibrary)
+  .get(getAllBooksByUsername);
 usersRouter
-    .route("/:username/wishlist")
-    .post(postBookWishList)
-    .get(getAllWishListByUsername);
+  .route("/:username/wishlist")
+  .post(postBookWishList)
+  .get(getAllWishListByUsername);
 usersRouter
-    .route("/:username/books/:bookid")
-    .get(getBookById)
-    .delete(deleteBookById);
+  .route("/:username/books/:bookid")
+  .get(getBookById)
+  .delete(deleteBookById);
 usersRouter
-    .route("/:username/wishlist/:bookid")
-    .get(getWishlistBookById)
-    .delete(deleteWishlistBookById);
+  .route("/:username/wishlist/:bookid")
+  .get(getWishlistBookById)
+  .delete(deleteWishlistBookById);
 usersRouter
-    .route("/:username/friendrequests")
-    .post(postFriendRequest)
-    .get(getFriendRequestsList);
+  .route("/:username/friendrequests")
+  .post(postFriendRequest)
+  .get(getFriendRequestsList);
 usersRouter.route("/:username/acceptfriend").post(postAceptFriendRequest);
 usersRouter
-    .route("/:username/friendrequests/:rejectfriend")
-    .delete(deleteFriendRequestById);
+  .route("/:username/friendrequests/:rejectfriend")
+  .delete(deleteFriendRequestById);
 usersRouter.route("/:username/friends").get(getFriendsList);
 usersRouter
-    .route("/:borrower/books/:bookid/requestlend/:owner")
-    .post(requestBookToBorrow);
+  .route("/:borrower/books/:bookid/requestlend/:owner")
+  .post(requestBookToBorrow);
 usersRouter
-    .route("/:owner/books/:bookid/requestlist/:borrower")
-    .get(getRequestsByBook);
+  .route("/:owner/books/:bookid/requestlist/:borrower")
+  .get(getRequestsByBook);
 usersRouter
-    .route("/:owner/books/:bookid/acceptrequest/:borrower")
-    .post(postAcceptedRequest);
+  .route("/:owner/books/:bookid/acceptrequest/:borrower")
+  .post(postAcceptedRequest);
 usersRouter.route("/:owner/lending").get(getLending);
 usersRouter
-    .route("/:username/borrowrequest/:bookid")
-    .delete(deleteBorrowRequest);
+  .route("/:username/borrowrequest/:bookid")
+  .delete(deleteBorrowRequest);
 usersRouter
-    .route("/:borrower/returnbook/:owner/:bookid")
-    .delete(returnBookById);
+  .route("/:borrower/returnbook/:owner/:bookid")
+  .delete(returnBookById);
 usersRouter.route("/:borrower/borrowing").get(getBorrowing);
 module.exports = usersRouter;
